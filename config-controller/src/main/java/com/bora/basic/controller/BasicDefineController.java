3d0afd3391403a0cfef4e31984ffc5b462be0f86
汪家磊 <wangjialei@boranet.com.cn>
2020-02-05 16:35:13 +0800

    暂时直接返回结果，等到公司时再对返回结果进行封装

--- config-controller/src/main/java/com/bora/basic/controller/BasicDefineController.java
+++ config-controller/src/main/java/com/bora/basic/controller/BasicDefineController.java
@@ -6,8 +6,8 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.bora.basic.dal.domain.BasicDefineDo;
 import com.bora.basic.service.service.IBasicDefineService;
-import com.bora.commmon.domain.Result;
-import com.bora.commmon.page.PageList;
+//import com.bora.commmon.domain.Result;
+//import com.bora.commmon.page.PageList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
@@ -34,9 +34,10 @@ public class BasicDefineController {
      * @return
      */
     @GetMapping("/getDefineByMark")
-    public Result<List<BasicDefineDo>> getDefineByMark(String mark){
+    public List<BasicDefineDo> getDefineByMark(String mark){
         List<BasicDefineDo> defineByMark = basicDefineService.getDefineByMark(mark);
-        return Result.ok(defineByMark);
+        return defineByMark;
+//        return Result.ok(defineByMark);
     }
 
     /**
@@ -46,11 +47,12 @@ public class BasicDefineController {
      * @return
      */
     @GetMapping("/listDefine")
-    public Result listDefine(@RequestParam(value = "pageIndex", required = false, defaultValue = "1") Integer pageIndex,
+    public IPage<BasicDefineDo> listDefine(@RequestParam(value = "pageIndex", required = false, defaultValue = "1") Integer pageIndex,
                                                   @RequestParam(value = "pageSize", required = false, defaultValue = "10") Integer pageSize){
         IPage<BasicDefineDo> page = new Page<>(pageIndex,pageSize);
         IPage<BasicDefineDo> defineDoIPage = basicDefineService.page(page);
-        return Result.ok(new PageList(new com.bora.commmon.page.Page(pageIndex,pageSize),defineDoIPage.getRecords()));
+        return defineDoIPage;
+//        return Result.ok(new PageList(new com.bora.commmon.page.Page(pageIndex,pageSize),defineDoIPage.getRecords()));
     }
 
     /**
@@ -59,12 +61,14 @@ public class BasicDefineController {
      * @return
      */
     @PostMapping("/saveDefine")
-    public Result saveDefine(@RequestBody String defineList){
+    public boolean saveDefine(@RequestBody String defineList){
         if(StringUtils.isEmpty(defineList)){
-            return Result.error("");
+//            return Result.error("");
+            return false;
         }
         List<BasicDefineDo> basicDefineDos = JSONObject.parseObject(defineList, List.class);
-        return Result.ok(basicDefineService.saveBatch(basicDefineDos));
+        return basicDefineService.saveBatch(basicDefineDos);
+//        return Result.ok(basicDefineService.saveBatch(basicDefineDos));
     }
 
     /**
@@ -73,8 +77,9 @@ public class BasicDefineController {
      * @return
      */
     @PostMapping("/deleteDefine")
-    public Result deleteDefine(@RequestParam("id") Long id){
-        return Result.ok(basicDefineService.removeById(id));
+    public boolean deleteDefine(@RequestParam("id") Long id){
+        return basicDefineService.removeById(id);
+//        return Result.ok(basicDefineService.removeById(id));
     }
 
     /**
@@ -83,12 +88,14 @@ public class BasicDefineController {
      * @return
      */
     @PostMapping("/editDefine")
-    public Result editDefine(@RequestBody String defineList){
+    public boolean editDefine(@RequestBody String defineList){
         if(StringUtils.isEmpty(defineList)){
-            return Result.error("");
+//            return Result.error("");
+            return false;
         }
         BasicDefineDo basicDefineDo = JSONObject.parseObject(defineList, BasicDefineDo.class);
-        return Result.ok(basicDefineService.updateById(basicDefineDo));
+        return basicDefineService.updateById(basicDefineDo);
+//        return Result.ok(basicDefineService.updateById(basicDefineDo));
     }
 
 
